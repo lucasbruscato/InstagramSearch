c85b7597c9310374328334952dd4a1feb19545ad
UOLCORP\lbruscato <lbruscato@uolinc.com>
2017-12-14 18:53:20 -0200

    atualizacao crawler

--- Search.py
+++ Search.py
@@ -1,27 +1,30 @@
 
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import time
 
-# from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 binary = FirefoxBinary('C:/Program Files (x86)/Mozilla Firefox/firefox.exe')
 driver = webdriver.Firefox(firefox_binary=binary)
+# driver = webdriver.Chrome()
 
 driver.get("https://www.instagram.com/")
 
-# webdriver.JavascriptExecutor.executeScript("static/bundles/4cb68b6771d9.js")
-# driver.execute_script("static/bundles/4cb68b6771d9.js")
+time.sleep(5)
 
-# driver.find_element_by_partial_link_text('a login').send_keys(Keys.RETURN)
-
-# driver.find_element_by_css_selector('p._g9ean').send_keys(Keys.RETURN)
-
-# JavascriptExecutor js = (JavascriptExecutor) driver;
-# js.executeScript("window.Search();");
-
-# js.executeScript("static/bundles/4cb68b6771d9.js");
+elem = driver.find_element_by_name("emailOrPhone")
+elem.send_keys("teste")
 
 
 
 
 
+
+
+
+
+
+
+
+# driver.execute_script("https://www.instagram.com/static/bundles/Vendor.js/4cb68b6771d9.js:1")
